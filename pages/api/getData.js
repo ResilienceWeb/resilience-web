8c9ad303a7a3b04ef5fb8c3013a2664059b47b13
DinerIsmail <ismail.diner@gmail.com>
2021-02-19 00:23:30 +0000

    Add error to response

--- pages/api/getData.js
+++ pages/api/getData.js
@@ -33,7 +33,7 @@ export default async (req, res) => {
 
 		res.status(200).json({ rows: data });
 	} catch (e) {
-		res.status(404).json({ error: 'Something went wrong' });
+		res.status(404).json({ error: 'Something went wrong' + e });
 		// eslint-disable-next-line no-console
 		console.error('Error: ', e);
 	}
