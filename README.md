72acbd7e6e455b0eaead532577d24e7119b140c9
DinerIsmail <ismail.diner@gmail.com>
2021-04-03 00:51:36 +0100

    Add utm tag to links to vercel.com

--- README.md
+++ README.md
@@ -16,4 +16,4 @@ Currently online at [https://cambridgeresilienceweb.org.uk](https://cambridgeres
 * Dependencies of this project are listed in package.json, and I will update them at a regular interval
 * For the network visualisation, I used react-graph-vis which is a wrapper around [vis.js](https://visjs.org/)
 
-![Powered by Vercel](https://cambridgeresilienceweb.org.uk/powered-by-vercel.svg)
+[![Powered by Vercel](https://cambridgeresilienceweb.org.uk/powered-by-vercel.svg)](https://vercel.com?utm_source=cambridge-resilience-web&utm_campaign=oss)
--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -4,15 +4,22 @@ import styles from './Footer.module.scss';
 
 const Footer = () => {
 	return (
-		<div className={styles.root}>
-			<span>Powered by</span>
-			<Image
-				alt="Powered by Vercel"
-				src="/vercel.svg"
-				width="85"
-				height="19"
-			/>
-		</div>
+		<a
+			href="https://vercel.com?utm_source=cambridge-resilience-web&utm_campaign=oss"
+			rel="noreferrer"
+			target="_blank"
+		>
+			<div className={styles.root}>
+				<span>Powered by</span>
+
+				<Image
+					alt="Powered by Vercel"
+					src="/vercel.svg"
+					width="85"
+					height="19"
+				/>
+			</div>
+		</a>
 	);
 };
 
