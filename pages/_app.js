b025104c0bff812919529c0e3b1365452e497d8e
DinerIsmail <ismail.diner@gmail.com>
2021-05-19 23:12:52 +0100

    Add custom tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -55,14 +55,8 @@ function App({ Component, pageProps }) {
 				<script
 					async
 					defer
-					src="https://scripts.withcabin.com/hello.js"
+					src="https://cabin.cambridgeresilienceweb.org.uk/hello.js"
 				></script>
-				{/* <link
-					href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-					rel="stylesheet"
-					integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-					crossOrigin="anonymous"
-				></link> */}
 			</Head>
 			<SafeHydrate>
 				<Provider
