080f3e1f62efb1c1fa53953dc985a67cc72e6852
DinerIsmail <ismail.diner@gmail.com>
2021-06-20 23:50:07 +0100

    Footer styling fixes

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -12,13 +12,13 @@ import Image from 'next/image';
 export default function Footer() {
 	return (
 		<Box
-			bg={useColorModeValue('gray.50', 'gray.900')}
-			color={useColorModeValue('gray.700', 'gray.200')}
+			bg={useColorModeValue('white', 'gray.800')}
+			color={useColorModeValue('gray.600', 'white')}
 			zIndex="6"
 		>
 			<Container
-				as={Stack}
-				maxW={'6xl'}
+				as={Flex}
+				maxW={'7xl'}
 				py={4}
 				direction={{ base: 'column', md: 'row' }}
 				spacing={4}
