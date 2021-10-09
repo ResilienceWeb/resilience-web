a4003475594b96fbd894168d2b416a5ee90a0dda
jonathon hawkins <jonathonhawkins92@gmail.com>
2021-10-09 10:24:02 +0100

    Creating VolunteerSwitch

---
+++ components/volunteer-switch/VolunteerSwitch.jsx
@@ -0,0 +1,28 @@
+import { chakra, FormLabel, Switch, Flex } from '@chakra-ui/react';
+
+const VolunteerSwitch = ({ handleSwitchChange, checked }) => {
+	return (
+		<chakra.div ml={2} mr={2}>
+			<Flex
+				direction="row"
+				alignItems="center"
+				justifyContent="center"
+				width="150px"
+			>
+				<FormLabel textAlign="center" fontSize="sm" mb={0}>
+					Volunteer
+				</FormLabel>
+				<Switch
+					isChecked={Boolean(checked)}
+					onChange={handleSwitchChange}
+					colorScheme="teal"
+					size="lg"
+					value={checked}
+					height="28px"
+				/>
+			</Flex>
+		</chakra.div>
+	);
+};
+
+export default VolunteerSwitch;
