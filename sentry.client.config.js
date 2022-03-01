a7eea4b6135d3c5d04d24d09a4233142c023e3f9
Diner <ismail.diner@gmail.com>
2022-03-01 00:35:37 +0000

    Fix issue with env variables

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,9 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-	dsn: SENTRY_DSN,
+	dsn:
+		SENTRY_DSN ||
+		'https://8bbdabe5bc7846b6be0fc5c2bf86ad6a@o1155559.ingest.sentry.io/6236055',
 	// Adjust this value in production, or use tracesSampler for greater control
 	tracesSampleRate: 1.0,
 	// ...
