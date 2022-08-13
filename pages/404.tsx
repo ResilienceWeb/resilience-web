76741771afd6d6c2d2b3a1c4512fd73040c18106
Diner <ismail.diner@gmail.com>
2022-08-13 23:42:57 +0100

    Remove usage of router in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,4 @@
-import { useRouter } from 'next/router'
-
 export default function Custom404() {
-    const router = useRouter()
-
-    void router.replace('/')
-
     return <h1>404 - Page Not Found</h1>
 }
 
