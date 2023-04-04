297d07ea4dce55afba9cf571592c9ba75260a084
Diner <ismail.diner@gmail.com>
2023-04-04 23:58:38 +0100

    Update WebCards.tsx

--- components/homepage/web-cards/WebCards.tsx
+++ components/homepage/web-cards/WebCards.tsx
@@ -1,11 +1,11 @@
 import {
   Container,
   Stack,
-  Heading,
-  Text,
+  // Heading,
+  // Text,
   Button,
-  Center,
-  Box,
+  // Center,
+  // Box,
   Link,
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
@@ -45,59 +45,59 @@ const WebCards = () => {
   )
 }
 
-/** Not used yet, realised we need to have the image fields for the webs first */
-const Card = () => {
-  return (
-    <Center py={6}>
-      <Box
-        maxW={'445px'}
-        w={'full'}
-        bg="white"
-        boxShadow={'2xl'}
-        rounded={'md'}
-        p={6}
-        overflow={'hidden'}
-      >
-        <Box
-          h={'210px'}
-          bg={'gray.100'}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          pos={'relative'}
-        >
-          <Image src={''} layout={'fill'} />
-        </Box>
-        <Stack>
-          <Text
-            color={'green.500'}
-            textTransform={'uppercase'}
-            fontWeight={800}
-            fontSize={'sm'}
-            letterSpacing={1.1}
-          >
-            Blog
-          </Text>
-          <Heading color="gray.700" fontSize={'2xl'} fontFamily={'body'}>
-            Boost your conversion rate
-          </Heading>
-          <Text color={'gray.500'}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-        </Stack>
-        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
-          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
-          </Stack>
-        </Stack>
-      </Box>
-    </Center>
-  )
-}
-
 export default WebCards
+
+/** Not used yet, realised we need to have the image fields for the webs first */
+// const Card = () => {
+//   return (
+//     <Center py={6}>
+//       <Box
+//         maxW={'445px'}
+//         w={'full'}
+//         bg="white"
+//         boxShadow={'2xl'}
+//         rounded={'md'}
+//         p={6}
+//         overflow={'hidden'}
+//       >
+//         <Box
+//           h={'210px'}
+//           bg={'gray.100'}
+//           mt={-6}
+//           mx={-6}
+//           mb={6}
+//           pos={'relative'}
+//         >
+//           <Image src={''} layout={'fill'} />
+//         </Box>
+//         <Stack>
+//           <Text
+//             color={'green.500'}
+//             textTransform={'uppercase'}
+//             fontWeight={800}
+//             fontSize={'sm'}
+//             letterSpacing={1.1}
+//           >
+//             Blog
+//           </Text>
+//           <Heading color="gray.700" fontSize={'2xl'} fontFamily={'body'}>
+//             Boost your conversion rate
+//           </Heading>
+//           <Text color={'gray.500'}>
+//             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+//             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+//             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+//             et ea rebum.
+//           </Text>
+//         </Stack>
+//         <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+//           <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+//             <Text fontWeight={600}>Achim Rolle</Text>
+//             <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
+//           </Stack>
+//         </Stack>
+//       </Box>
+//     </Center>
+//   )
+// }
 
