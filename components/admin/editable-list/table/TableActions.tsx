0d37b02c6b26883b3cba3b1cc829ad2daac8b6c0
Diner <ismail.diner@gmail.com>
2023-08-09 12:47:41 +0100

    Fix issue where add listing button was hidden for owners

--- components/admin/editable-list/table/TableActions.tsx
+++ components/admin/editable-list/table/TableActions.tsx
@@ -12,6 +12,7 @@ import { HiOutlineSearch, HiPlus } from 'react-icons/hi'
 import { useCategories } from '@hooks/categories'
 import customMultiSelectStyles from '@styles/select-styles'
 import { useHasPermissionForCurrentWeb } from '@hooks/permissions'
+import { useIsOwnerOfCurrentWeb } from '@hooks/ownership'
 
 const TableActions = ({
   searchTerm,
@@ -20,6 +21,7 @@ const TableActions = ({
   goToCreateListing,
 }) => {
   const hasPermissionForCurrentWeb = useHasPermissionForCurrentWeb()
+  const isOwnerOfCurrentWeb = useIsOwnerOfCurrentWeb()
 
   const { categories: fetchedCategories } = useCategories()
   const [categories, setCategories] = useState<any[]>()
@@ -50,54 +52,55 @@ const TableActions = ({
         md: 'auto',
       }}
     >
-      {hasPermissionForCurrentWeb && (
-        <>
-          <HStack spacing="1rem">
-            <InputGroup minW="250px">
-              <Select
-                isMulti
-                isSearchable={false}
-                onChange={handleSelectedCategoriesChange}
-                options={categories}
-                placeholder="Filter by category"
-                styles={customMultiSelectStyles}
-              />
-            </InputGroup>
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                color="gray.400"
-                fontSize="xl"
-              >
-                <HiOutlineSearch />
-              </InputLeftElement>
-              <Input
-                placeholder="Search"
-                onChange={handleSearchTermChange}
-                style={{
-                  backgroundColor: '#ffffff',
-                }}
-                rounded="base"
-                value={searchTerm}
-                maxHeight="38px"
-              />
-            </InputGroup>
-          </HStack>
-          <Button
-            bg="rw.700"
-            colorScheme="rw.700"
-            iconSpacing="1"
-            leftIcon={<HiPlus fontSize="1.25em" />}
-            onClick={goToCreateListing}
-            variant="solid"
-            size="md"
-            maxHeight="36px"
-            _hover={{ bg: 'rw.900' }}
-          >
-            New listing
-          </Button>
-        </>
-      )}
+      {hasPermissionForCurrentWeb ||
+        (isOwnerOfCurrentWeb && (
+          <>
+            <HStack spacing="1rem">
+              <InputGroup minW="250px">
+                <Select
+                  isMulti
+                  isSearchable={false}
+                  onChange={handleSelectedCategoriesChange}
+                  options={categories}
+                  placeholder="Filter by category"
+                  styles={customMultiSelectStyles}
+                />
+              </InputGroup>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents="none"
+                  color="gray.400"
+                  fontSize="xl"
+                >
+                  <HiOutlineSearch />
+                </InputLeftElement>
+                <Input
+                  placeholder="Search"
+                  onChange={handleSearchTermChange}
+                  style={{
+                    backgroundColor: '#ffffff',
+                  }}
+                  rounded="base"
+                  value={searchTerm}
+                  maxHeight="38px"
+                />
+              </InputGroup>
+            </HStack>
+            <Button
+              bg="rw.700"
+              colorScheme="rw.700"
+              iconSpacing="1"
+              leftIcon={<HiPlus fontSize="1.25em" />}
+              onClick={goToCreateListing}
+              variant="solid"
+              size="md"
+              maxHeight="36px"
+              _hover={{ bg: 'rw.900' }}
+            >
+              New listing
+            </Button>
+          </>
+        ))}
     </Stack>
   )
 }
