893028e0555ad3a982dbf4b3726647163f88401f
Diner <ismail.diner@gmail.com>
2023-09-16 09:45:32 +0100

    Remove nodemailer types

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ module.exports = withSentryConfig(
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
     // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+    transpileClientSDK: false,
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: '/monitoring',
     // Hides source maps from generated client bundles
