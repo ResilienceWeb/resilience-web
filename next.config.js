9dec0b8b69a67dbd22c965a5316f5c290784bc8d
Diner <ismail.diner@gmail.com>
2023-09-21 19:42:15 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -28,7 +28,10 @@ const nextConfig = {
       {
         source: '/api/feedback',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { 'key': 'Access-Control-Allow-Credentials', 'value': 'true' },
+          { 'key': 'Access-Control-Allow-Origin', 'value': '*' },
+          { 'key': 'Access-Control-Allow-Methods', 'value': 'GET,DELETE,PATCH,POST,PUT' },
+          { 'key': 'Access-Control-Allow-Headers', 'value': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, baggage, sentry-trace' }
         ]
       }
     ]
