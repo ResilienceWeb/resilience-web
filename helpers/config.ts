58262b1b1cdf449e2c0dc43fc9f0c7eda3ba369f
Diner <ismail.diner@gmail.com>
2023-11-13 22:46:36 +0000

    Fix Prettier issues

--- helpers/config.ts
+++ helpers/config.ts
@@ -2,15 +2,15 @@ export const REMOTE_URL =
   process.env.VERCEL_ENV === 'preview'
     ? `https://${process.env.VERCEL_URL}`
     : process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://resilienceweb.org.uk'
+      ? 'http://localhost:3000'
+      : 'https://resilienceweb.org.uk'
 
 export const REMOTE_HOSTNAME =
   process.env.VERCEL_ENV === 'preview'
     ? process.env.VERCEL_URL
     : process.env.NODE_ENV === 'development'
-    ? 'localhost:3000'
-    : 'resilienceweb.org.uk'
+      ? 'localhost:3000'
+      : 'resilienceweb.org.uk'
 
 export const PROTOCOL =
   process.env.NODE_ENV === 'development' ? 'http' : 'https'
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -97,4 +97,3 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     }
   }
 }
-
