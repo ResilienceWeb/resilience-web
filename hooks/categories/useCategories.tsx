8daf9837cf09c50450496ff8b321191eec98590b
Diner <ismail.diner@gmail.com>
2024-03-03 14:03:16 +0000

    Update useCategories.tsx

--- hooks/categories/useCategories.tsx
+++ hooks/categories/useCategories.tsx
@@ -34,7 +34,15 @@ export default function useCategories() {
   })
 
   return {
-    categories,
+    categories: categories.sort((category1, category2) => {
+      if (category1.label < category2.label) {
+        return -1
+      }
+      if (category1.label > category2.label) {
+        return 1
+      }
+      return 0
+    }),
     isPending,
     isError,
   }
