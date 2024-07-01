04a8371cffd8a5723167a892a0014ff2def226b5
Diner <ismail.diner@gmail.com>
2024-07-01 15:12:53 +0100

    Move latitude and longitude into separate model

--- hooks/listings/useListings.tsx
+++ hooks/listings/useListings.tsx
@@ -7,6 +7,7 @@ async function fetchListingsRequest({ queryKey }) {
   const response = await fetch(`/api/listings?web=${webSlug}`)
   const data = await response.json()
   const { listings } = data
+  console.log({ listings })
   return listings
 }
 
