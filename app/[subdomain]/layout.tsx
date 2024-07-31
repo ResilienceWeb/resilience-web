731c3935da4bb3b74ba3209535061469b6a75564
Diner <ismail.diner@gmail.com>
2024-07-31 09:58:59 +0100

    Update layout.tsx

--- app/[subdomain]/layout.tsx
+++ app/[subdomain]/layout.tsx
@@ -3,13 +3,16 @@ import { Suspense } from 'react'
 import { QueryParamProvider } from 'use-query-params'
 import NextAdapterApp from 'next-query-params/app'
 import StoreProvider from '@store/StoreProvider'
+import { ReCaptchaProvider } from 'next-recaptcha-v3'
 import '@styles/vis-network-simplified.css'
 
 export default function Layout({ children }) {
   return (
     <Suspense>
       <QueryParamProvider adapter={NextAdapterApp}>
-        <StoreProvider>{children}</StoreProvider>
+        <StoreProvider>
+          <ReCaptchaProvider>{children}</ReCaptchaProvider>
+        </StoreProvider>
       </QueryParamProvider>
     </Suspense>
   )
