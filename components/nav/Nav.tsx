1e1d0f614f14d8968a423eb2f36c9bdb35b2d5b1
Diner <ismail.diner@gmail.com>
2024-07-31 21:58:09 +0100

    Update Nav.tsx

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -72,7 +72,7 @@ export default function MainNav() {
       },
       {
         label: 'News',
-        href: `${REMOTE_URL}/about`,
+        href: `${REMOTE_URL}/news`,
       },
       {
         label: 'Knowledgebase',
