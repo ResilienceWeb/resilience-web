168dcb9eccd67fab13e31becb5c16366f7b6868f
Diner <ismail.diner@gmail.com>
2024-09-04 22:53:35 +0100

    Fix image uploading issue

--- components/admin/listing-form/ListingForm.tsx
+++ components/admin/listing-form/ListingForm.tsx
@@ -153,9 +153,6 @@ const ListingForm = ({ categories, listing, handleSubmit }: Props) => {
   }, [listing?.relations])
 
   const handleSubmitForm = (data) => {
-    if (data.image) {
-      delete data.image
-    }
     if (data.location) {
       data.latitude = data.location.latitude
       data.longitude = data.location.longitude
