de47e20ecf5a6f3f240237353666f1f7efa2985c
Diner <ismail.diner@gmail.com>
2024-09-15 16:36:34 +0100

    Update route.ts

--- app/api/cron/deploy/route.ts
+++ app/api/cron/deploy/route.ts
@@ -1,4 +1,4 @@
-export default async function handler() {
+export async function GET() {
   if (!process.env.DEPLOY_URL) {
     throw new Error(
       'You must define MONDAY_DEPLOY_URL env variable for this cron.',
