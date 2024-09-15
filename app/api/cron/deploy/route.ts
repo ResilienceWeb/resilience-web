cbeb89f25e2b0e4e7d2abe33d6902dff80b14729
Diner <ismail.diner@gmail.com>
2024-09-15 16:28:28 +0100

    Set up cron job to deploy every morning

---
+++ app/api/cron/deploy/route.ts
@@ -0,0 +1,8 @@
+export default async function handler() {
+  if (!process.env.DEPLOY_URL) {
+    throw new Error(
+      'You must define MONDAY_DEPLOY_URL env variable for this cron.',
+    )
+  }
+  return await fetch(process.env.DEPLOY_URL)
+}
