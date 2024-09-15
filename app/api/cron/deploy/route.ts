57cec0bfeec5fb7b5ad13265a6e473b25fb60687
Diner <ismail.diner@gmail.com>
2024-09-15 22:09:09 +0100

    Delete route.ts

--- app/api/cron/deploy/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  if (!process.env.DEPLOY_URL) {
-    throw new Error(
-      'You must define MONDAY_DEPLOY_URL env variable for this cron.',
-    )
-  }
-  return await fetch(process.env.DEPLOY_URL)
-}
