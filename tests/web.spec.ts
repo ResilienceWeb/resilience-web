b87dd670eac3f41a6e49e7893715d7307b7b47f3
Diner <ismail.diner@gmail.com>
2024-09-29 22:18:56 +0100

    Add E2E tests (#212)

---
+++ tests/web.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test'
+
+test('Web title is displayed', async ({ page }) => {
+  await page.goto('http://durham.localhost:3000')
+
+  await expect(
+    page.getByRole('heading', { level: 2, exact: true, name: 'Durham' }),
+  ).toBeVisible()
+})
+
+test('Listings are loaded', async ({ page }) => {
+  await page.goto('http://durham.localhost:3000')
+
+  await expect(page.getByText('Conservation Research Institute')).toBeVisible()
+  await expect(page.getByText('Durham Community Kitchen')).toBeVisible()
+})
