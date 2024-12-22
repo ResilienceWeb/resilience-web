08358839b2248959447a5afd0840398e197e9d32
Diner <ismail.diner@gmail.com>
2024-12-22 13:08:48 +0000

    Set up Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
