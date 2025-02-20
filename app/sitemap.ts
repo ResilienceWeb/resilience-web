1a08671e325fa0214474ff6d5c3407037d39f3de
Diner <ismail.diner@gmail.com>
2025-02-20 15:03:47 +0000

    Use new endpoint for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,15 @@
-// import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next'
 
-export default function sitemap() {
-  // const response = await fetch(`https://resilienceweb.org.uk/api/listings`)
-  // const data = await response.json()
-  // const { listings } = data
-  // const pathItems = listings
-  //   .filter((l) => l.web.published === true)
-  //   .filter((l) => l.image !== null)
-  //   .filter((l) => l.description !== '')
-  //   .map((l) => ({
-  //     url: `https://${l.web.slug}.resilienceweb.org.uk/${l.slug}`,
-  //     lastModified: l.updatedAt,
-  //   }))
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const response = await fetch(
+    `https://resilienceweb.org.uk/api/listings/for-sitemap`,
+  )
+  const data = await response.json()
+  const { listings } = data
+  const pathItems = listings.map((l) => ({
+    url: `https://${l.web.slug}.resilienceweb.org.uk/${l.slug}`,
+    lastModified: l.updatedAt,
+  }))
 
   return [
     {
@@ -23,6 +21,6 @@ export default function sitemap() {
     {
       url: 'https://resilienceweb.org.uk/news',
     },
-    // ...pathItems,
+    ...pathItems,
   ]
 }
