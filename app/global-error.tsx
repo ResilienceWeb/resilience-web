d417076e388c0d5be512bd71cbce27a15be21ac8
Diner <ismail.diner@gmail.com>
2025-03-01 22:11:37 +0000

    Remove Sentry (#256)

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,19 +1,13 @@
 'use client' // Error boundaries must be Client Components
 
-import * as Sentry from '@sentry/nextjs'
 import NextError from 'next/error'
-import { useEffect } from 'react'
 
 export default function GlobalError({
-  error,
+  _error,
 }: {
-  error: Error & { digest?: string }
+  _error: Error & { digest?: string }
   reset: () => void
 }) {
-  useEffect(() => {
-    Sentry.captureException(error)
-  }, [error])
-
   return (
     <html lang="en">
       <body>
