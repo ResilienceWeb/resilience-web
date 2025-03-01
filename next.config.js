8d31779b02395de70d798682097680dd03876661
Diner <ismail.diner@gmail.com>
2025-03-01 22:08:11 +0000

    Remove Sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 // @ts-check
 
-import { withSentryConfig } from '@sentry/nextjs'
-
 /**
  * @type {import('next').NextConfig}
  */
@@ -80,7 +78,7 @@ const nextConfig = {
           {
             key: 'Access-Control-Allow-Headers',
             value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, baggage, sentry-trace',
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, baggage',
           },
         ],
       },
@@ -88,22 +86,4 @@ const nextConfig = {
   }
 }
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    org: 'resilience-web',
-    project: 'resilience-web',
-    telemetry: false,
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    // Suppresses source map uploading logs during build
-    silent: true,
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: false,
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    // tunnelRoute: '/monitoring',
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  },
-)
+export default nextConfig
