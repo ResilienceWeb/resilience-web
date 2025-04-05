320cfc1c19007afc4865026ef698fc649a16d1f5
Diner <ismail.diner@gmail.com>
2025-04-05 23:14:31 +0100

    Temporary change to fix build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,14 @@
 import type { MetadataRoute } from 'next'
-import { REMOTE_URL } from '@helpers/config'
+// import { REMOTE_URL } from '@helpers/config'
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const response = await fetch(`${REMOTE_URL}/api/listings/for-sitemap`)
-  const data = await response.json()
-  const { listings } = data
-  const pathItems = listings.map((l) => ({
-    url: `https://${l.web.slug}.resilienceweb.org.uk/${l.slug}`,
-    lastModified: l.updatedAt,
-  }))
+export default function sitemap(): MetadataRoute.Sitemap {
+  // const response = await fetch(`${REMOTE_URL}/api/listings/for-sitemap`)
+  // const data = await response.json()
+  // const { listings } = data
+  // const pathItems = listings.map((l) => ({
+  //   url: `https://${l.web.slug}.resilienceweb.org.uk/${l.slug}`,
+  //   lastModified: l.updatedAt,
+  // }))
 
   return [
     {
@@ -20,6 +20,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: 'https://resilienceweb.org.uk/news',
     },
-    ...pathItems,
+    // ...pathItems,
   ]
 }
