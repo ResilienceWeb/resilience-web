d9899743c27585194cfd6310d0f5549282e9e273
Diner <ismail.diner@gmail.com>
2025-08-27 21:17:28 +0100

    Remove legacy admin field from users table

---
+++ prisma/migrations/20250827201653_remove_admin_field_from_users_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `admin` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."users" DROP COLUMN "admin";
