637e9f191d9bb69a7da62e14d0b10cf89be216db
Diner <ismail.diner@gmail.com>
2025-10-03 17:31:09 +0100

    Update client.ts

--- prisma/client.ts
+++ prisma/client.ts
@@ -6,6 +6,11 @@ const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: process.env.PRISMA_LOG_LEVEL ? ['info'] : [],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   })
 
 if (process.env.NODE_ENV !== 'production') {
