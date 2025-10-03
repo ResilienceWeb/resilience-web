a88eb6ff35eb76c3c91ae130947cdec7ec974b74
Diner <ismail.diner@gmail.com>
2025-10-03 16:29:49 +0100

    Update client.ts

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,7 +2,11 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['info'],
+  })
 
 if (process.env.NODE_ENV === 'development') {
   globalForPrisma.prisma = prisma
