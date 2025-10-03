9bd9c53ab1c1cf596461e6af60414333cd26619a
Diner <ismail.diner@gmail.com>
2025-10-03 16:16:48 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -55,6 +55,7 @@ const nextConfig = {
   },
   experimental: {
     scrollRestoration: process.env.NODE_ENV === 'development' ? false : true,
+    staticGenerationMaxConcurrency: 5
   },
   async rewrites() {
     return [
