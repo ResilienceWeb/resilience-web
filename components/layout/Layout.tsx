e91853aa95b21247692559382ef49b160372eac2
Diner <ismail.diner@gmail.com>
2025-10-11 18:41:33 +0100

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -30,13 +30,18 @@ const Layout = ({
       <main
         className={`flex min-h-[calc(100vh-186px)] flex-col items-center md:min-h-[calc(100vh-328px)] ${!hideBorder ? 'border-t border-t-gray-200' : ''} `}
       >
-        {pathname === '/' && (
+        {/* {pathname === '/' && (
           <AlertBanner
             content="Join our online monthly Assembly. Click here to read more."
             type="info"
             url="https://www.eventbrite.com/e/resilience-web-monthly-assembly-tickets-1220916868219?aff=oddtdtcreator"
           />
-        )}
+        )} */}
+        <AlertBanner
+          content="We are currently migrating to a new host. There may be issues temporarily with our website, please bear with us."
+          type="warning"
+          url="https://www.eventbrite.com/e/resilience-web-monthly-assembly-tickets-1220916868219?aff=oddtdtcreator"
+        />
         <div
           className={`flex w-full max-w-6xl flex-col items-center justify-center px-4 pt-6 pb-8 ${applyPostStyling ? styles.root : ''} `}
         >
