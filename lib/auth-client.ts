8c258e3b60ea1dd632e1cb78521501e5e22dfedb
Diner <ismail.diner@gmail.com>
2025-10-12 09:51:17 +0100

    Attempt to fix signout issue

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,6 @@ import { adminClient, magicLinkClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   plugins: [adminClient(), magicLinkClient()],
 })
 
