883b92cfcda7b016e6f66a488b9faefc2f0eddec
Diner <ismail.diner@gmail.com>
2025-11-08 18:21:59 +0000

    Switch Prisma to use new client

--- hooks/categories/useCategoriesPublic.tsx
+++ hooks/categories/useCategoriesPublic.tsx
@@ -1,4 +1,4 @@
-import type { Category } from '@prisma/client'
+import type { Category } from '@prisma-client'
 import { useQuery } from '@tanstack/react-query'
 
 async function fetchCategoriesRequest({ queryKey }) {
