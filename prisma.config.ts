a1f889f359691c0d6edf5f267200e685c3a2ca04
Diner <ismail.diner@gmail.com>
2025-11-26 19:45:17 +0000

    Fix env var

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'prisma/config'
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
+  schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: env('DATABASE_URL'),
   },
   migrations: {
     path: 'prisma/migrations',
