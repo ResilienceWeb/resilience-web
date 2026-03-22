2fc1b974531221bd764bf0e3487dcfcd11aeb99f
Diner <ismail.diner@gmail.com>
2026-03-22 23:31:25 +0000

    Revert tanstaack query upgrade + remove unused import

--- components/listing/Listing.tsx
+++ components/listing/Listing.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { memo, useMemo } from 'react'
-import { FiEdit, FiMapPin, FiNavigation } from 'react-icons/fi'
+import { FiEdit, FiMapPin } from 'react-icons/fi'
 import { HiArrowLeft, HiUserGroup, HiExternalLink } from 'react-icons/hi'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
