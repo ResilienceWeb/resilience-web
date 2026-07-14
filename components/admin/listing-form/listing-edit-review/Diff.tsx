9cd9e76b5e4b01759e71de8a595b513c9facd0ee
Diner <ismail.diner@gmail.com>
2026-07-14 10:26:18 +0100

    Update Diff.tsx

--- components/admin/listing-form/listing-edit-review/Diff.tsx
+++ components/admin/listing-form/listing-edit-review/Diff.tsx
@@ -14,24 +14,27 @@ const styles = {
 
 type Props = {
   label?: string
-  string1: string
-  string2: string
+  string1: string | null | undefined
+  string2: string | null | undefined
   mode?: 'characters' | 'words'
 }
 
 export default function Diff({
   label,
-  string1 = '',
-  string2 = '',
+  string1,
+  string2,
   mode = 'words',
 }: Props) {
-  if (!string1 && !string2) return null
-  if (string1 === string2) return null
+  const str1 = string1 ?? ''
+  const str2 = string2 ?? ''
+
+  if (!str1 && !str2) return null
+  if (str1 === str2) return null
 
   let groups = []
 
-  if (mode === 'characters') groups = diff.diffChars(string1, string2)
-  if (mode === 'words') groups = diff.diffWords(string1, string2)
+  if (mode === 'characters') groups = diff.diffChars(str1, str2)
+  if (mode === 'words') groups = diff.diffWords(str1, str2)
 
   const mappedNodes = groups.map((group, index) => {
     const { value, added, removed } = group
@@ -49,7 +52,7 @@ export default function Diff({
     <div>
       {label && <h3 className="mt-2 text-lg font-semibold">{label}</h3>}
       <span>{mappedNodes}</span>
-      {string1 !== '' && string2 === '' && (
+      {str1 !== '' && str2 === '' && (
         <span className="ml-1 inline text-gray-600 italic">(deleted)</span>
       )}
     </div>
