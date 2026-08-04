b0bf89ed18780f1c8446668f70e7136fa77da053
Diner <ismail.diner@gmail.com>
2026-08-04 21:58:36 +0100

    Update dependencies

--- prisma/generated/internal/prismaNamespace.ts
+++ prisma/generated/internal/prismaNamespace.ts
@@ -80,11 +80,11 @@ export type PrismaVersion = {
 }
 
 /**
- * Prisma Client JS version: 7.9.0
+ * Prisma Client JS version: 7.9.1
  * Query Engine version: e922089b7d7502aff4249d5da3420f6fa55fc6ad
  */
 export const prismaVersion: PrismaVersion = {
-  client: "7.9.0",
+  client: "7.9.1",
   engine: "e922089b7d7502aff4249d5da3420f6fa55fc6ad"
 }
 
